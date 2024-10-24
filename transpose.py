6a5bfa24757345c624e35e8bf89dd176f6965a1c
Yakultea7 <86728826+Yakultea7@users.noreply.github.com>
2024-10-24 19:17:22 +0800

    Update transpose.py
    可讀取目前程序路徑中的指定資料集
    將第一橫排index刪除，並將index字串中的特殊字元清空

--- transpose.py
+++ transpose.py
@@ -1,22 +1,49 @@
 import pandas as pd
+import re
+from pathlib import Path
 
-# 讀取 CSV 檔案
-file_path = 'H:\\lab\\24_t=20s_30s.csv' 
-chunk=58
+# 取得目前程序的工作路徑
+current_directory = Path.cwd()
+
+# 要存取的檔案名稱
+file_name = '24_t=20s_30s.csv'
+
+# 拼接完整的路徑
+file_path = current_directory / file_name
+
+chunk = 58
+
+# 定義清理函數
+def clean_string(value):
+    if isinstance(value, str):  # 確保處理的數據是字串
+        # 刪除 "QXX"、空白字符，並刪除結尾的冒號
+        return re.sub(r'Q\d+\s*:\s*|\s+|:$', '', value)
+    return value  # 如果不是字串則原樣返回
 
 for i in range(21):
-    df = pd.read_csv(file_path,skiprows=365+(chunk*i)+i*2,nrows=chunk,header=None) #讀取每區塊
 
-    selected_rows = df.iloc[[0,1, 2, 3, 4, 12 , 13] , :] #取得需要的參數
+    df = pd.read_csv(file_path, skiprows=365 + (chunk * i) + i * 2, nrows=chunk, header=None)  # 讀取每區塊
 
-    CF_values = pd.to_numeric(df.iloc[26, 1:]) #取得合力數列
-    CF_mean = CF_values.mean() #計算合力平均
-    comparison_result = (CF_values > CF_mean).astype(int) #比較
+    # 取得需要的參數
+    selected_rows = df.iloc[[0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 13], :]
 
-    selected_T = selected_rows.T #轉置
-    selected_T['FC_Result'] = comparison_result.T #轉置力鏈判斷加進去
+    # 取得總力數列
+    TF_values = pd.to_numeric(df.iloc[52, 1:])
+    TF_mean = TF_values.mean()  # 計算總力平均
+    comparison_result = (TF_values > TF_mean).astype(int)  # 比較
+    comparison_result.loc[0] = "FC"  # index 第1列填入 FC
 
-    output_file = f'TIME_{200+i*5}.csv'
-    selected_T.to_csv(output_file, index=False)
-    
+    # 轉置
+    selected_T = selected_rows.T
+
+    # 將力鏈判斷加進去
+    selected_T['comparison_result'] = comparison_result
+
+    # 對轉置後的第一行進行正規表示式處理
+    selected_T.loc[0, :] = selected_T.loc[0, :].apply(clean_string)
+
+    # 輸出為 CSV 文件
+    output_file = f'TIME_{200 + i * 5}.csv'
+    selected_T.to_csv(output_file, index=False, header=False)
+
     print(f'Saved: {output_file}')
